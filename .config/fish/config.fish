6564b775a8a17c76781db90c585a27a736dc8d1e
ledsun <shigeru.nakajima@gmail.com>
2021-11-05 17:09:17 +0900

    set propt style

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,31 +1,3 @@
-function fish_prompt
-    printf '~ '
-end
-
-# function fish_right_prompt
-#    git_prompt
-#    set_color magenta
-#    printf ' '
-#    printf '%s' (echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||')
-# end
-
-function git_prompt
-    if git rev-parse --show-toplevel >/dev/null 2>&1
-        set_color green
-        printf '%s' (git_current_branch)
-        set_color normal
-    end
-end
-
-function git_current_branch -d 'Prints a human-readable representation of the current branch'
-  set -l ref (git symbolic-ref HEAD ^/dev/null; or git rev-parse --short HEAD ^/dev/null)
-  if test -n "$ref"
-    echo $ref | sed -e s,refs/heads/,,
-    return 0
-  end
-end
-
-
 # tabtab source for serverless package
 # uninstall by removing these lines or running `tabtab uninstall serverless`
 [ -f /Users/shigerunakajima/seven-trumpets/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/shigerunakajima/seven-trumpets/node_modules/tabtab/.completions/serverless.fish
