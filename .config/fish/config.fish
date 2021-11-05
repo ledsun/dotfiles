8bff7de305a0c07669b192576a036b1968b5cb2f
ledsun <shigeru.nakajima@gmail.com>
2021-11-05 17:10:33 +0900

    update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,6 @@
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /Users/shigerunakajima/seven-trumpets/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/shigerunakajima/seven-trumpets/node_modules/tabtab/.completions/serverless.fish
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /Users/shigerunakajima/seven-trumpets/node_modules/tabtab/.completions/sls.fish ]; and . /Users/shigerunakajima/seven-trumpets/node_modules/tabtab/.completions/sls.fish
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/shigerunakajima/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/shigerunakajima/Downloads/google-cloud-sdk/path.fish.inc'; end
-
 # for RubyMine
 set PATH $HOME/.rbenv/bin/ $PATH
 set PATH /usr/local/bin/ $PATH
 
+# for rbenv
 status --is-interactive; and source (rbenv init -|psub)
