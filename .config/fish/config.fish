2f98d5fd4950b7710787fecbb2ef8d79029b87eb
shigeru.nakajima <shigeru.nakajima@gmail.com>
2026-03-28 16:42:53 +0900

    Update fish to 4.5

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
 # for RubyMine
-set PATH $HOME/.rbenv/bin/ $PATH
-set PATH /usr/local/bin/ $PATH
+fish_add_path $HOME/.rbenv/bin
 
-# for rbenv
-status --is-interactive; and source (rbenv init -|psub)
+# for wasmtime
+set -gx WASMTIME_HOME "$HOME/.wasmtime"
+fish_add_path $WASMTIME_HOME/bin
 
+if status is-interactive
+    rbenv init - | source
+    direnv hook fish | source
+end
